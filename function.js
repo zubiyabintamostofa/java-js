0df0a8e2fa12741315afa615bd3976120b903d8b
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-01-29 23:31:53 +0600

    new

---
+++ function.js
@@ -0,0 +1,13 @@
+//function key word er por functioner nam likhte hoi
+//function er nam er por () bebohar kortee hoi
+//function declearation:
+function startFan(){
+    console.log('walk the switch')
+    console.log('press the switch')
+}
+//function call
+startFan();
+console.log('eat');
+console.log('drink');
+startFan();
+startFan();
---
+++ recap.js
@@ -0,0 +1,19 @@
+//var booklist=['bof','ff','sdj','sra']
+//var ffIndex=booklist.indexOf('ff');
+//notun add korte chaile
+//booklist[1]='himu';//er mane holo 1 no index e himu nam asbe ager boi er nam chole jbe;
+
+// console.log(ffIndex);
+ //console.log(booklist);
+ //booklist.push('rupa');
+
+booklist.push('rupa');
+ console.log(booklist);
+
+//while loop
+var i=1;//loop variabble
+while (i<15){
+    console.log(i);
+ 
+    i++;
+}

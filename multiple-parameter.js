5708659f548c1738f6a1ad5a52665b28e99e1e9d
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-01-30 10:57:25 +0600

    alu

---
+++ multiple-parameter.js
@@ -0,0 +1,20 @@
+function addTwoNums(num1,num2){
+    console.log(num1,num2);
+    var total=num1+num2;
+    return total;
+}
+var firstNum=22;
+var secNum=66;
+var result= addTwoNums(firstNum,secNum);
+console.log('result value:',result);
+
+//multiple er niyom
+function multiplyTwoNums(num1,num2){
+    console.log(num1,num2);
+    var total=num1*num2;
+    return total;
+}
+var firstNum=11;
+var secNum=11;
+var result=multiplyTwoNums(firstNum,secNum);
+console.log('result value:',result);

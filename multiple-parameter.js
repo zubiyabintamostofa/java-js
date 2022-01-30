2a73890b25027f50028a431662056d5e719db59d
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-01-30 11:37:39 +0600

    done

--- multiple-parameter.js
+++ multiple-parameter.js
@@ -18,3 +18,15 @@ var firstNum=11;
 var secNum=11;
 var result=multiplyTwoNums(firstNum,secNum);
 console.log('result value:',result);
+
+//biyog
+function subtractionTwoNums(num1,num2)
+{console.log(num1,num2);
+    var total=num1-num2;
+    return total;
+}
+
+var firstNum=11;
+var secNum=22;
+var result=subtractionTwoNums(firstNum,secNum);
+console.log('result value:',result);
